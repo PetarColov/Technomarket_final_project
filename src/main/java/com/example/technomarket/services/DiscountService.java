bfa8ae30101826064a80ebeef6602ebdc26579ec
YoanaBalcheva <ibalcheva74@gmail.com>
2022-10-27 22:40:32 +0300

    removing a commented method from discount service

--- src/main/java/com/example/technomarket/services/DiscountService.java
+++ src/main/java/com/example/technomarket/services/DiscountService.java
@@ -29,52 +29,6 @@ public class DiscountService {
     @Autowired
     ModelMapper modelMapper;
 
-//    public ResponseDiscountDTO addDiscount(RequestDiscountDTO requestDiscountDTO) {
-//        LocalDate startAt = requestDiscountDTO.getStartedAt();
-//        LocalDate endAt = requestDiscountDTO.getEndedAt();
-//        int discountPercent = requestDiscountDTO.getDiscountPercent();
-//        String discountDescription = requestDiscountDTO.getDiscountDescription();
-//
-//        if(endAt.isBefore(startAt)){
-//            throw new BadRequestException("Invalid duration entered!");
-//        }
-//
-//        if(startAt.isBefore(LocalDate.now())){
-//            throw new BadRequestException("This date has already passed!");
-//        }
-//
-//        Optional<Discount> discountOptional = discountRepository.findByDiscountDescriptionAndDiscountPercent(discountDescription,discountPercent);
-//
-//        if(discountOptional.isPresent()){
-//            Discount discount = discountOptional.get();
-//            for(Long pid : requestDiscountDTO.getProducts()){
-//                Product product = getProduct(pid);
-////                discount.getProducts().add(product);
-//               // discountRepository.save(discount);
-//                product.setDiscount(discount);
-//                productRepository.save(product);
-//            }
-//            //discountRepository.save(discount);
-//        }
-//        else{
-//            Discount discount = modelMapper.map(requestDiscountDTO, Discount.class);
-//            for(Long pid : requestDiscountDTO.getProducts()){
-//                Product product = getProduct(pid);
-//                //discount.getProducts().add(product);
-//               // discountRepository.save(discount);
-//                product.setDiscount(discount);
-//                productRepository.save(product);
-//            }
-//        }
-//
-//        Optional<Discount> discountOpt = discountRepository.findByDiscountDescriptionAndDiscountPercent(discountDescription,discountPercent);
-//        Discount discountFromDB = discountOpt.get();
-//        ResponseDiscountDTO responseDiscountDTO = modelMapper.map(discountFromDB,ResponseDiscountDTO.class);
-//        responseDiscountDTO.setProducts(discountFromDB.getProducts().stream().map(p -> modelMapper.map(p, ProductForClientDTO.class)).collect(Collectors.toSet()));
-//        return responseDiscountDTO;
-//    }
-
-
     private Product getProduct(Long pid){
         Optional<Product> product = productRepository.findById(pid);
         if(product.isPresent()){
